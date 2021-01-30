496d4f484a0035b5c4e249d04fa3817efe190cc7
Victoria Newton <vnewton999@yahoo.com>
2021-01-29 17:21:31 -0700

    timedComputation before debugging

--- TimedComutation.java
+++ TimedComutation.java
@@ -21,7 +21,28 @@ public class TimedComputation {
     System.out.print("A triangle with sides 42 and 17 has hypontenuse");
     System.out.println(hypontenuse);
 
-    System.out.println(\nMathimatically, sin(x)*sin(x))
+    System.out.println("\nMathimatically, sin(x)*sin(x) + " + "cos(x)*cos(x) - 1 should be 0.");
+    System.out.println("Let's check this for x = 100:");
+    System.out.print("    sin(100)*sin(100) + cos(100)*cos(100) - 1 is: ");
+    System.out.println(Math.sin(100)*Math.sin(100) + Math.cos(100)*Math.cos(100) - 1);
+    System.out.println("(There can be round-off errors when" + " computing with real numbers!)");
+    
+    System.out.print("\nHere is a random number:   ");
+    System.out.println( Math.random());
+
+    System.out.print("\nThe value of Math.PI is ");
+    System.out.println(Math.PI );
+
+    endTime = System.nanoTime();
+    compTime = endTime - startTime;
+    seconds = compTime / 1000000000.0;
+
+    System.out.print("\nRun time in nanoseconds was: ");
+    System.out.println(comptime);
+    System.out.println("(This is probably not perfectly accurate!");
+    System.out.println("\nRun time in seconds was:  ");
+    System.out.print("seconds");
+
   }
 
 }
