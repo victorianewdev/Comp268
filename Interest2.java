88be922dffbfadbe8c3371c99ea4bba9a5068db7
Victoria Newton <vnewton999@yahoo.com>
2021-02-02 12:28:00 -0700

    pre debug of Interest2

---
+++ Interest2.java
@@ -0,0 +1,28 @@
+/**
+ * This class implements a simple program that will compute the amount of interest that is earned on an investment over a period of a year. The initial amount of the investment and the interest rate are input by the user.
+ * The value of the investment at the end of the year is output. the rate must be input as a decimal, not a percentage (for example, 0.05 rather than 5).
+ */
+
+ import textio.TextIO;
+
+ public class Interest2 {
+
+    public static void main (String[] args) {
+
+        double principal;       // The value of the investment
+        double rate;            // The annual interest rate.
+        double interest;        // The interest earned during the year.
+
+        System.out.print("Enter the initial investment: ");
+        principal = TextIO.getlnDouble();
+
+        System.out.print("Enter the annual interest rate (as a decimal): ");
+        rate = TextIO.getlnDouble();
+
+        interest = principal * rate;        // Compute this year's interest.
+        principal = principal + interest;   // Add it to principal.
+
+        System.out.printf("The amount of interest is $%1.2%n", interest);
+        System.out.printf("The value after one year is $%1.2%n", principal);
+    }
+ }
