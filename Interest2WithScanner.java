15f0607dfcd2700dad93e06002aeedfb01e8c25f
Victoria Newton <vnewton999@yahoo.com>
2021-02-10 18:15:51 -0700

    create and begin Interest2WithScanner, to practice with package and class Scanner

---
+++ Interest2WithScanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Interest2WithScanner{
+    public static void main(Strings[] args){
+        Scanner stdin = new Scanner(System.in);
+
+        double principal;       // The value of the investment.
+        double rate;            // The annual interest rate.
+        double interest;        // The interest earned during the year.
+
+        System.out.print("enter the intitial investment:  ");
+        
+    }
+}
