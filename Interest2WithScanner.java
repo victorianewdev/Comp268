ad44f0f6900ff28519d1c2d7ae8ee761558b6444
Victoria Newton <vnewton999@yahoo.com>
2021-02-10 18:33:11 -0700

    Update Interest2WithScanner, debug

--- Interest2WithScanner.java
+++ Interest2WithScanner.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Interest2WithScanner{
-    public static void main(Strings[] args){
+    public static void main(String[] args){
         Scanner stdin = new Scanner(System.in);
 
         double principal;       // The value of the investment.
@@ -9,6 +9,18 @@ public class Interest2WithScanner{
         double interest;        // The interest earned during the year.
 
         System.out.print("enter the intitial investment:  ");
+        principal = stdin.nextDouble();
         
+
+        System.out.print("Enter the annual intreste rate (as a decimal):  ");
+        rate = stdin.nextDouble();
+
+        interest = principal * rate;        // Compute this year's interest.
+        principal = principal + interest;   // Add it to principal.
+
+        System.out.printf("The amount of interest is $%1.2f%n", interest);
+        System.out.printf("The value after one year is $%1.2f%n", principal);
+        
+        stdin.close();
     }
 }
